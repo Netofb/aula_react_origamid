1942cd8ee0c2d2258881a62ee7d5b520c1438999
Fabio Barros lima <fabi.neto.fb@gmail.com>
2024-04-18 06:34:11 -0300

    aula

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 
+import ReactDom from 'react-dom/client';
+import React from 'react';
+import App from './App';
 
 
 
+
+
+
+ReactDom.createRoot(document.getElementById("root")).render(<App />);
 
 
 
