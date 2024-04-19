353244e7ad2503927a8de7bb99cc4db5153c9e9f
Fabio Barros lima <fabi.neto.fb@gmail.com>
2024-04-19 06:43:56 -0300

    aula

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,49 @@
 
-import React from 'react'
+import React from 'react';
 
 
 const Button = () => {
   return(
     <button> Teste</button>
-  )
-}
+  );
+};
 const Form = () => {
   return (
     <>
     <label htmlFor='nome'>Nome</label>
     <input type='text' id='nome' />
     </>
-  )
-}
+  );
+};
+
+const titulo = <h1>Esse é um titulo</h1>;
+const Chaves = () =>{
+  const random = Math.random();
+  const ativo = true;
+
+  return(
+    <>
+    {titulo}
+    <p className={ativo ? 'ativo' : 'inativo'}>{(random*100 )/ 50}</p>
+    </>
+  );
+};
+
+
+
+
+
 
 
 
 const App = () => {
  
   return (
-    <><Button /><Form /></>
-  )
-}
+    <><Button />
+    <Form />
+    <Chaves />
+    </>
+  );
+};
 
 export default App
