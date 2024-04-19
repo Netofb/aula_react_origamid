ae815c2c1251e5b4531e2b4807c412db927179af
Fabio Barros lima <fabi.neto.fb@gmail.com>
2024-04-19 06:31:38 -0300

    aula

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,21 @@ const Button = () => {
     <button> Teste</button>
   )
 }
-
+const Form = () => {
+  return (
+    <>
+    <label htmlFor='nome'>Nome</label>
+    <input type='text' id='nome' />
+    </>
+  )
+}
 
 
 
 const App = () => {
  
   return (
-    <Button />
-    
+    <><Button /><Form /></>
   )
 }
 
