91a7e23d7e298c151a3eb669a5bd8b03bb15a7e7
Fabio Barros lima <fabi.neto.fb@gmail.com>
2024-04-22 06:32:03 -0300

    aula

--- src/App.jsx
+++ src/App.jsx
@@ -34,45 +34,45 @@ import React from 'react';
 // Não utilize CSS externo, use o style para mudar as cores
 // Se a situação estiver ativa pinte de verde, inativa vermelho
 // Se o gasto for maior que 10000 mostre uma mensagem
-const luana = {
-  cliente: 'Luana',
-  idade: 27,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-  ],
-  ativa: true,
-};
+// const luana = {
+//   cliente: 'Luana',
+//   idade: 27,
+//   compras: [
+//     { nome: 'Notebook', preco: 'R$ 2500' },
+//     { nome: 'Geladeira', preco: 'R$ 3000' },
+//     { nome: 'Smartphone', preco: 'R$ 1500' },
+//   ],
+//   ativa: true,
+// };
 
-const mario = {
-  cliente: 'Mario',
-  idade: 31,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-    { nome: 'Guitarra', preco: 'R$ 3500' },
-  ],
-  ativa: false,
-};
+// const mario = {
+//   cliente: 'Mario',
+//   idade: 31,
+//   compras: [
+//     { nome: 'Notebook', preco: 'R$ 2500' },
+//     { nome: 'Geladeira', preco: 'R$ 3000' },
+//     { nome: 'Smartphone', preco: 'R$ 1500' },
+//     { nome: 'Guitarra', preco: 'R$ 3500' },
+//   ],
+//   ativa: false,
+// };
 
-const App = () => {
-  const dados = mario;
+// const App = () => {
+//   const dados = mario;
 
-  const total = dados.compras
-  .map((item) => Number(item.preco.replace('R$ ', '')))
-  .reduce((a, b) => a + b);
+//   const total = dados.compras
+//   .map((item) => Number(item.preco.replace('R$ ', '')))
+//   .reduce((a, b) => a + b);
 
-  return <div>
-    <p>Nome: {dados.cliente}</p>
-    <p>Idade: {dados.idade}</p>
-    <p style={{ color: dados.ativa ? 'green' : 'red' }}>Situação: {dados.ativa ? 'ativo' : 'inativo'}</p>
-    <p>Nome: {dados.cliente}</p>
-     <p>Total: R$ {total}</p>
-      {total > 10000 && <p>Você está gastando muito</p>}
-  </div>;
-};
+//   return <div>
+//     <p>Nome: {dados.cliente}</p>
+//     <p>Idade: {dados.idade}</p>
+//     <p style={{ color: dados.ativa ? 'green' : 'red' }}>Situação: {dados.ativa ? 'ativo' : 'inativo'}</p>
+//     <p>Nome: {dados.cliente}</p>
+//      <p>Total: R$ {total}</p>
+//       {total > 10000 && <p>Você está gastando muito</p>}
+//   </div>;
+// };
 
 
 
