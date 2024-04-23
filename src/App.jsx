ee8d795fea40f20d2ca4e4616063f6c1b965b93d
Fabio Barros lima <fabi.neto.fb@gmail.com>
2024-04-23 06:03:16 -0300

    atividade

--- src/App.jsx
+++ src/App.jsx
@@ -74,16 +74,40 @@ import React from 'react';
 //   </div>;
 // };
 
-const Filmes = () =>{
-  const filmes = ['Before Sunrise', 'Before Sunset', 'Before Midnight'];
-  return(
-    <ul>
-      {filmes.map((filme) =>(
-        <li key={filme}>{filme}</li>
-      ))}
-    </ul>
-  )
-}
+// const Filmes = () =>{
+//   const filmes = ['Before Sunrise', 'Before Sunset', 'Before Midnight'];
+//   return(
+//     <ul>
+//       {filmes.map((filme) =>(
+//         <li key={filme}>{filme}</li>
+//       ))}
+//     </ul>
+//   )
+// }
+
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
+
 
 
 
@@ -96,8 +120,8 @@ const App = () => {
     {/* <Button />
     <Form />
     <Chaves /> */}
-    <Filmes />
-
+    {/* <Filmes /> */}
+    <section></section>
 
 
 
