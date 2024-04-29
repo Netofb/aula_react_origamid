a3399cc13b7d07ac64f658e784178b9fb08cd38f
Fabio Barros lima <fabi.neto.fb@gmail.com>
2024-04-29 06:14:50 -0300

    aula

--- src/App.jsx
+++ src/App.jsx
@@ -180,7 +180,23 @@ import Home from './Home';
 // };
 
 
+const App = () => {
+  
+  const ativoHook = React.useState(false);
+  const ativoValor = ativoHook[0];
+  const atualizaValor = ativoHook[1];
+  console.log(ativoValor);
 
+    
+
+  function handleClick(){
+    atualizaValor(!ativoValor)
+  }
+
+  return(
+    <button onClick={handleClick}>{ativoValor ? 'Ativo' : 'Inativo'}</button>
+  )
+}
 
 
 
