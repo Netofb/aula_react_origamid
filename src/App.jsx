c398501ab1cf155f3c9621bf7036ea7c4783cf62
Fabio Barros lima <fabi.neto.fb@gmail.com>
2024-05-02 06:27:07 -0300

    aula

--- src/App.jsx
+++ src/App.jsx
@@ -236,11 +236,28 @@ import Modal from './Modal';
 // };
 
 
+// const App = () => {
+//   const [ativo, setAtivo] = React.useState(true);
+
+//   function handleClick() {
+//     // usando um callback
+//     setAtivo((anterior) => !anterior);
+//   }
+
+//   return (
+//     <button onClick={handleClick}>
+//       {ativo ? 'Está Ativo' : 'Está Inativo'}
+//     </button>
+//   );
+// };
 const App = () => {
-  const [ativo, setAtivo] = React.useState(true);
+  // Callback no estado inicial, só será executado na criação do componente
+  const [ativo, setAtivo] = React.useState(() => {
+    const ativoLocal = window.localStorage.getItem('ativo');
+    return ativoLocal;
+  });
 
   function handleClick() {
-    // usando um callback
     setAtivo((anterior) => !anterior);
   }
 
