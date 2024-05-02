a25ef36fffebeb5a7e0523c29686f389711a8cca
Fabio Barros lima <fabi.neto.fb@gmail.com>
2024-05-02 06:23:30 -0300

    aula

--- src/App.jsx
+++ src/App.jsx
@@ -211,31 +211,45 @@ import Modal from './Modal';
 //   );
 // }
 
+// const App = () => {
+//   const [items, setItems] = React.useState(['Item 1', 'Item 2']);
+
+//   function handleClick() {
+//     // Errado. Modifique o estado apenas com a função de atualização (setItems)
+//     items.push('Novo Item');
+//   }
+
+//   function handleClickReativo() {
+//     // Correto. Eu desestruturo a array atual, criando uma nova e adiciono um novo elemento
+//     setItems([...items, 'Novo Item']);
+// }
+
+//   return (
+//     <>
+//       {items.map((item, i) => (
+//         <li key={i}>{item}</li>
+//       ))}
+//       <button onClick={handleClick}>Adicionar Item</button>
+//       <button onClick={handleClickReativo}>Adicionar Reativo</button>
+//     </>
+//   );
+// };
+
+
 const App = () => {
-  const [items, setItems] = React.useState(['Item 1', 'Item 2']);
+  const [ativo, setAtivo] = React.useState(true);
 
   function handleClick() {
-    // Errado. Modifique o estado apenas com a função de atualização (setItems)
-    items.push('Novo Item');
+    // usando um callback
+    setAtivo((anterior) => !anterior);
   }
 
-  function handleClickReativo() {
-    // Correto. Eu desestruturo a array atual, criando uma nova e adiciono um novo elemento
-    setItems([...items, 'Novo Item']);
-}
-
   return (
-    <>
-      {items.map((item, i) => (
-        <li key={i}>{item}</li>
-      ))}
-      <button onClick={handleClick}>Adicionar Item</button>
-      <button onClick={handleClickReativo}>Adicionar Reativo</button>
-    </>
+    <button onClick={handleClick}>
+      {ativo ? 'Está Ativo' : 'Está Inativo'}
+    </button>
   );
 };
-
-
 
 
 
